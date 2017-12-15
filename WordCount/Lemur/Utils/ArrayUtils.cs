24610a1c20041980d8dfbeab0667804c43d52b3a
lerpinglemur <19180097+lerpinglemur@users.noreply.github.com>
2017-12-14 20:00:33 -0800

    ArrayUtils,

---
+++ WordCount/Lemur/Utils/ArrayUtils.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lemur.Utils {
+
+	public static class ArrayUtils {
+
+		/// <summary>
+		/// Unsafe index-of operation which can be used when an array is known
+		/// to be non-empty.
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="a"></param>
+		/// <param name="v"></param>
+		/// <returns>The index of the given value, or -1 if none is found.</returns>
+		static public int UnsafeIndexOf<T>( T[] a, T v ) where T : struct {
+
+			int len = a.Length;
+			for( int i = 0; i < len; i++ ) {
+
+				if( v.Equals( a[i] ) ) {
+					return i;
+				}
+
+			}
+			return -1;
+
+		} //
+
+		/// <summary>
+		/// Unsafe Contains operation does not check that the array parameter
+		/// is non-null.
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="a"></param>
+		/// <param name="v"></param>
+		/// <returns></returns>
+		static public bool UnsafeContains<T>( T[] a, T v ) where T : struct {
+
+			int len = a.Length;
+			for( int i = 0; i < len; i++ ) {
+
+				if( v.Equals( a[i] ) ) {
+					return true;
+				}
+
+			}
+			return false;
+
+		} //
+
+	} // class
+
+} // namespace
---
+++ WordCount/Lemur/WordCount/WordCount.cs
@@ -0,0 +1,105 @@
+﻿using Lemur.Utils;
+using System;
+
+namespace Lemur.WordCount {
+
+	/// <summary>
+	/// Base utility class for counting words.
+	/// </summary>
+    public class WordCount {
+
+		static public int CountSentences( string source ) {
+
+			if( string.IsNullOrEmpty( source ) ) {
+				return 0;
+			}
+
+			char[] terminators = { '.', '!', '?' };
+
+			int len = source.Length;
+			int index = 0;
+			int count = 0;
+
+			while( index < len ) {
+
+				char c = source[index++];
+
+				if( ArrayUtils.UnsafeContains( terminators, c ) ) {
+
+					count++;
+					// advance until a non-terminator is found.
+					while( index < len && ArrayUtils.UnsafeContains( terminators, source[index] ) ) {
+						index++;
+					}
+
+				}
+
+			} //
+
+			return count;
+
+		} //
+
+		/// <summary>
+		/// Count the number of words in a source string.
+		/// </summary>
+		/// <param name="source"></param>
+		/// <returns></returns>
+		static public int CountWords( string source ) {
+
+			if( string.IsNullOrEmpty( source ) ) {
+				return 0;
+			}
+
+			//int count = 0;
+			char[] whitespace = { ' ', '\t', '\n' };
+			return source.Split( whitespace, StringSplitOptions.RemoveEmptyEntries ).Length;
+
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="source"></param>
+		/// <param name="wordSeparators">Characters which count as separating words.</param>
+		/// <returns></returns>
+		static public int CountWords( string source, char[] wordSeparators ) {
+
+			if( string.IsNullOrEmpty( source ) ) {
+				return 0;
+			}
+
+			return source.Split( wordSeparators, StringSplitOptions.RemoveEmptyEntries ).Length;
+
+		} //
+
+		/// <summary>
+		/// Counts the number of characters in a source string.
+		/// </summary>
+		/// <param name="source"></param>
+		/// <returns></returns>
+		static public int CountChars( string source ) {
+
+			if( string.IsNullOrEmpty( source ) ) {
+				return 0;
+			}
+
+			int count = 0;
+
+			int len = source.Length;
+			for( int i = 0; i < len; i++ ) {
+
+				char c = source[i];
+				if( !char.IsWhiteSpace( c ) ) {
+					count++;
+				}
+
+			} // for-loop.
+
+			return count;
+
+		}
+
+    } // class
+
+} // namespace
